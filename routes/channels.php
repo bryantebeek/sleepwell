421e7107d7ca7b4bd091795c226651bafe23df07
Justin van Elst <justinvanelst@gmail.com>
2023-02-10 22:31:30 +0100

    Add pusher (wip)

--- routes/channels.php
+++ routes/channels.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Broadcast;
 Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
 });
+Broadcast::channel('story', function ($user, $orderId) {
+    //return $user->id === Story::findOrNew($storyId)->user_id;
+});
