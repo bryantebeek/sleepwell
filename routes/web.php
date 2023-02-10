1f096c149be1a2ca6d404b477bdfe2aa572f96a7
Justin van Elst <justinvanelst@gmail.com>
2023-02-10 21:01:21 +0100

    Add openai api

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use OpenAI\Laravel\Facades\OpenAI;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,6 +18,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('about-php', function () {
+    $result = OpenAI::completions()->create([
+        'model'  => 'text-davinci-003',
+        'prompt' => 'PHP is',
+    ]);
+    dd($result);
+});
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
