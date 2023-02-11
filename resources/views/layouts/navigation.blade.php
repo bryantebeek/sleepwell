dee638c4843db8ba241804debe24f9e789ff9c0f
Bryan te Beek <bryantebeek@gmail.com>
2023-02-11 13:23:39 +0100

    fix styling

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -11,12 +11,16 @@
                 </div>
 
                 <!-- Navigation Links -->
-                <div class="flex space-x-8 ml-10 ">
-                    <x-nav-link :href="route('stories.index')" :active="request()->routeIs('stories.index')">
+                <div class="flex space-x-8 ml-10 navigation">
+                    <x-nav-link :href="route('stories.index')" :active="request()->routeIs('stories.index')" class="flex">
+                        Sleepwell
+                    </x-nav-link>
+
+                    <x-nav-link :href="route('stories.index')" :active="request()->routeIs('stories.index')" class="hidden sm:flex">
                         Stories
                     </x-nav-link>
 
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')" class="hidden sm:flex">
                         Profile
                     </x-nav-link>
                 </div>
@@ -71,8 +75,8 @@
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('story')">
-                Story
+            <x-responsive-nav-link :href="route('stories.index')" :active="request()->routeIs('story')">
+                Stories
             </x-responsive-nav-link>
 
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
