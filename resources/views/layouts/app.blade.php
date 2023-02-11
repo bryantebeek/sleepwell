e1e5a0a06fbd7e0326650776582e96ba12d0708c
Bryan te Beek <bryantebeek@gmail.com>
2023-02-11 10:05:51 +0100

    fix(viewport): do not allow zoom for now, sorryyyy

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
 
         <meta name="application-name" content="{{ config('app.name') }}">
         <meta name="csrf-token" content="{{ csrf_token() }}">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
 
         <title>Sleepwell</title>
 
