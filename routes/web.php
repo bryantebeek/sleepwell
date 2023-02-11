3eadeb46728fc462de6d4055a1797052febdeea1
Justin van Elst <justinvanelst@gmail.com>
2023-02-11 14:39:29 +0100

    cleanup routes

--- routes/web.php
+++ routes/web.php
@@ -28,49 +28,10 @@ Route::get('/stories', function () {
 
 Route::get('/stories/{story}', function (Story $story) {
 
-
-
-
     return view('story')
         ->with('story', $story)
         ->with('showNavigation', false);
 })->name('stories.view');
-
-Route::get('about-php', function () {
-    $result = OpenAI::completions()->create([
-        'model'  => 'text-davinci-003',
-        'prompt' => 'PHP is',
-    ]);
-    dd($result);
-});
-Route::get('random-prompts', function () {
-    $prompts    = Cache::get('prompts');
-    $promptName = rtrim(Arr::random($prompts), ".blade.php");
-    $this->user = auth()->user();
-
-    $prompt = view('prompts.' . $promptName, $this->user->profile)->render();
-    dd($prompt);
-});
-
-Route::get('generate-story', function () {
-
-    $storyGenerator = new \App\Services\GenerateStory(
-        auth()->user(),
-        [
-//            'story'=>'The train ride was long and tiring, but the excitement of James and his brother Luke was palpable. James couldn’t wait to explore the new world outside the window. Click-clack, click-clack, went the wheels of the train. He stuck his head out and felt the wind on his face. “Weeeee!” he shouted. His brother Luke just smiled and shook his head.
-//
-//James couldn’t help but want to join the birds who were flying by. He jumped up and down and waved his arms wildly, hoping to take off. Luke smiled and just watched.
-//
-//James noticed a few passengers looking out at him, but he didn’t care. He was lost in his own world. He made funny faces, blew raspberries, and pretended to be a superhero. Luke laughed and started to join in on the fun.
-//
-//The train ride was full of laughter and silliness. James and Luke created a whole new world on the train, and the miles flew by in no time. Click-clack, click-clack, went the wheels of the train.'
-//      ,
-//            'summary'=>'Train ride, laughter, silliness, exploration.'
-    ]
-    );
-    dd($storyGenerator->story->toArray());
-});
-
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -83,3 +44,38 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+//Route::get('about-php', function () {
+//    $result = OpenAI::completions()->create([
+//        'model'  => 'text-davinci-003',
+//        'prompt' => 'PHP is',
+//    ]);
+//    dd($result);
+//});
+//Route::get('random-prompts', function () {
+//    $prompts    = Cache::get('prompts');
+//    $promptName = rtrim(Arr::random($prompts), ".blade.php");
+//    $this->user = auth()->user();
+//
+//    $prompt = view('prompts.' . $promptName, $this->user->profile)->render();
+//    dd($prompt);
+//});
+//
+//Route::get('generate-story', function () {
+//
+//    $storyGenerator = new \App\Services\GenerateStory(
+//        auth()->user(),
+//        [
+////            'story'=>'The train ride was long and tiring, but the excitement of James and his brother Luke was palpable. James couldn’t wait to explore the new world outside the window. Click-clack, click-clack, went the wheels of the train. He stuck his head out and felt the wind on his face. “Weeeee!” he shouted. His brother Luke just smiled and shook his head.
+////
+////James couldn’t help but want to join the birds who were flying by. He jumped up and down and waved his arms wildly, hoping to take off. Luke smiled and just watched.
+////
+////James noticed a few passengers looking out at him, but he didn’t care. He was lost in his own world. He made funny faces, blew raspberries, and pretended to be a superhero. Luke laughed and started to join in on the fun.
+////
+////The train ride was full of laughter and silliness. James and Luke created a whole new world on the train, and the miles flew by in no time. Click-clack, click-clack, went the wheels of the train.'
+////      ,
+////            'summary'=>'Train ride, laughter, silliness, exploration.'
+//    ]
+//    );
+//    dd($storyGenerator->story->toArray());
+//});
