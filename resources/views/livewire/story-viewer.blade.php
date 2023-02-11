12ca6c5f16d10d570894e0766e622c6d6fa649ac
Bryan te Beek <bryantebeek@gmail.com>
2023-02-11 10:57:27 +0100

    feat(story): add loading state for images

--- resources/views/livewire/story-viewer.blade.php
+++ resources/views/livewire/story-viewer.blade.php
@@ -14,7 +14,8 @@
                     @if ($step > 0)
                         <button
                             class="inline-block px-6 py-2.5 bg-purple-200 text-purple-900 font-medium text-xs leading-tight rounded hover:bg-purple-300 active:bg-purple-300 focus:bg-purple-300 focus:outline-none transition duration-150 ease-in-out"
-                            wire:click="previous">
+                            wire:click="previous"
+                            x-click="document.getElementById('story-image').classList.add('loading')">
                             Previous
                         </button>
                     @endif
@@ -28,7 +29,8 @@
                     @if ($step < count($beats) - 1)
                         <button
                             class="inline-block px-6 py-2.5 bg-purple-200 text-purple-900 font-medium text-xs leading-tight rounded hover:bg-purple-300 active:bg-purple-300 focus:bg-purple-300 focus:outline-none transition duration-150 ease-in-out"
-                            wire:click="next">
+                            wire:click="next"
+                            x-click="document.getElementById('story-image').classList.add('loading')">
                             Next
                         </button>
                     @endif
@@ -45,7 +47,15 @@
         </div>
     </div>
 
-    <div class="image rounded-2xl">
-        <img src="{{ $currentBeat['image'] }}" class="transition duration-150 ease-in-out"/>
+    <div class="image rounded-2xl loading" id="story-image">
+        <div class="loader" id="loader">
+            {{-- https://github.com/n3r4zzurr0/svg-spinners/tree/main/preview --}}
+            <svg width="36" height="36" fill="#fff" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><style>.spinner_Wezc{transform-origin:center;animation:spinner_Oiah .75s step-end infinite}@keyframes spinner_Oiah{8.3%{transform:rotate(30deg)}16.6%{transform:rotate(60deg)}25%{transform:rotate(90deg)}33.3%{transform:rotate(120deg)}41.6%{transform:rotate(150deg)}50%{transform:rotate(180deg)}58.3%{transform:rotate(210deg)}66.6%{transform:rotate(240deg)}75%{transform:rotate(270deg)}83.3%{transform:rotate(300deg)}91.6%{transform:rotate(330deg)}100%{transform:rotate(360deg)}}</style><g class="spinner_Wezc"><circle cx="12" cy="2.5" r="1.5" opacity=".14"/><circle cx="16.75" cy="3.77" r="1.5" opacity=".29"/><circle cx="20.23" cy="7.25" r="1.5" opacity=".43"/><circle cx="21.50" cy="12.00" r="1.5" opacity=".57"/><circle cx="20.23" cy="16.75" r="1.5" opacity=".71"/><circle cx="16.75" cy="20.23" r="1.5" opacity=".86"/><circle cx="12" cy="21.5" r="1.5"/></g></svg>
+        </div>
+
+        <img src="{{ $currentBeat['image'] }}"
+             class="transition duration-150 ease-in-out"
+             onload="document.getElementById('story-image').classList.remove('loading')"
+        />
     </div>
 </div>
