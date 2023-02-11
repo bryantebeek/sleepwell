c44ac2584932b6a30e95493109e30b0dda792d14
Bryan te Beek <bryantebeek@gmail.com>
2023-02-11 12:19:14 +0100

    feat(profile): improvements

--- resources/views/livewire/story-viewer.blade.php
+++ resources/views/livewire/story-viewer.blade.php
@@ -50,8 +50,10 @@
     <div class="image rounded-2xl loading" id="story-image">
         <div class="loader" id="loader">
             {{-- https://github.com/n3r4zzurr0/svg-spinners/tree/main/preview --}}
-            <svg width="36" height="36" fill="#fff" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><style>.spinner_Wezc{transform-origin:center;animation:spinner_Oiah .75s step-end infinite}@keyframes spinner_Oiah{8.3%{transform:rotate(30deg)}16.6%{transform:rotate(60deg)}25%{transform:rotate(90deg)}33.3%{transform:rotate(120deg)}41.6%{transform:rotate(150deg)}50%{transform:rotate(180deg)}58.3%{transform:rotate(210deg)}66.6%{transform:rotate(240deg)}75%{transform:rotate(270deg)}83.3%{transform:rotate(300deg)}91.6%{transform:rotate(330deg)}100%{transform:rotate(360deg)}}</style><g class="spinner_Wezc"><circle cx="12" cy="2.5" r="1.5" opacity=".14"/><circle cx="16.75" cy="3.77" r="1.5" opacity=".29"/><circle cx="20.23" cy="7.25" r="1.5" opacity=".43"/><circle cx="21.50" cy="12.00" r="1.5" opacity=".57"/><circle cx="20.23" cy="16.75" r="1.5" opacity=".71"/><circle cx="16.75" cy="20.23" r="1.5" opacity=".86"/><circle cx="12" cy="21.5" r="1.5"/></g></svg>
+            @include('partials.loader')
         </div>
+
+        <img class="overlay" src="https://uc01fedb6bc2936a1cddc8977b17.previews.dropboxusercontent.com/p/thumb/ABymM1xsxO9wVS2jejMaIoLmiQ3wUdnsVjYVveURchfAWHgTQ3fFJWuXFa60Xa1akYv6xs0kZpYUJkj-GFsrNgzDpsooVSsifke1p3ZiuUpgUJdOflX7f2uBTsO-vMBn4LyUsSwsu8BO9pWMqhrC-AC8VaISbwC482EYiROdD_myy4hYEZv0wVh_ujbX-MMLbW0DmhlPH43DlYKVxpvegTv0ymZdxuAftTT9W2jjSffx6NVTG0LkmCNanDEiy6SrxLwm-BL8wZuih8LWm5AficA-M3Ik9PjdWldRNoc8EA6rbfbVBXAWreyzBoo8rIeWAcEDfU4EGgIS623Qz-37kYBeiP_GiNHR--R1Um2RRB311heJ1MxrXK0RlbmUwjE9D24/p.png" />
 
         <img src="{{ $currentBeat['image'] }}"
              class="transition duration-150 ease-in-out"
